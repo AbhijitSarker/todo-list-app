4e672a675539006fd7bda9a75a76738f580595ec
AbhijitSarker <abhijitsarker03@gmail.com>
2023-12-06 00:07:39 +0600

    basic folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+import { router } from './routes/Routes';
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
