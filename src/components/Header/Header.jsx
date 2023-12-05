57efca418fe5a1de8eb4eca84bc107fb1c3a27e7
AbhijitSarker <abhijitsarker03@gmail.com>
2023-12-06 02:05:21 +0600

    header secton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            header
+        <div className='bg-[#040C18] text-white font-serif'>
+            <div className='container mx-auto py-5 flex items-center justify-between'>
+                <h1 className='text-5xl font-bold '>TODO LIST</h1>
+                <h3>Hello User</h3>
+            </div>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="container mx-auto my-20 font-serif gradient__text">
+            <input className="border py-2 px-4 rounded-md" type="text" placeholder={'Add your Task'} />
+            <button className="border py-2 px-4 rounded-md">Add Task</button>
         </div>
     );
 };
